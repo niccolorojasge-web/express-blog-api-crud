6484eef78dd4da7da700e5033018d3b80d96fb27
Niccolo Rojas <niccolo.rojas.ge@gmail.com>
2026-02-09 16:49:47 +0100

    modiche su posts

---
+++ routes/posts.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router();
+const postController = require ('../controller/postController')
+
+//rotte di crud
+
+//index
+ router.get('/',postController.index );
+ //show
+ router.get('/:id', postController.show);
+//store
+router.post('/',postController.store);
+//update
+router.put('/:id', postController.update);
+//modify
+router.patch('/:id', postController.modify);
+//delete
+router.delete('/:id',postController.delet);
+
+module.exports = router;
