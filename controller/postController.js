4407447ef5dc2d68eb2df0b9239eb69371c9afbc
Niccolo Rojas <niccolo.rojas.ge@gmail.com>
2026-02-09 19:57:16 +0100

    formattato

--- controller/postController.js
+++ controller/postController.js
@@ -1,52 +1,52 @@
 const posts = require('../data/posts');
 //funzione index
-function index(req, res){
-//inizialmente il menu filtrato corrisponde a quello originale
+function index(req, res) {
+    //inizialmente il menu filtrato corrisponde a quello originale
     let filteredPost = posts;
-    if(req.query.posted){
-        filteredPost= posts.filter(
-            post=>post.posted.includes(req.query.posted)
+    if (req.query.posted) {
+        filteredPost = posts.filter(
+            post => post.posted.includes(req.query.posted)
         );
     }
     res.json(filteredPost)
-    };
+};
 //show
-function show (req, res){
+function show(req, res) {
     const id = parseInt(req.params.id)
-    const post = posts.find(post=> post.id === id);
-    if(!post){
+    const post = posts.find(post => post.id === id);
+    if (!post) {
         res.status(404)
         return res.json({
-            error:"not found",
-            message :"post non trovato" 
+            error: "not found",
+            message: "post non trovato"
         })
     }
     res.json(post)
 };
 //store
-function store (req, res){
-  res.send('creazione nuovo post');
+function store(req, res) {
+    res.send('creazione nuovo post');
 };
 //update
-function update(req, res){
-    res.send('modifica integrale del post'+ req.params.id);
+function update(req, res) {
+    res.send('modifica integrale del post' + req.params.id);
 };
 //modify
-function modify(req,res){
-res.send('modifica parziale post'+ req.params.id);
+function modify(req, res) {
+    res.send('modifica parziale post' + req.params.id);
 };
 //delete
-function delet(req,res){
-const id = parseInt(req.params.id)
-const post = posts.find(post=>post.id===id)
-if(!post){
-    return res.json({
-         error:"not found",
-            message :"post non trovato"
-    })
-}
-posts.splice(posts.indexOf(post),1);
-console.log(posts);
-res.status(204)
+function delet(req, res) {
+    const id = parseInt(req.params.id)
+    const post = posts.find(post => post.id === id)
+    if (!post) {
+        return res.json({
+            error: "not found",
+            message: "post non trovato"
+        })
+    }
+    posts.splice(posts.indexOf(post), 1);
+    console.log(posts);
+    res.status(204)
 };
-module.exports={index, show, store , update,modify,delet}
+module.exports = { index, show, store, update, modify, delet }
